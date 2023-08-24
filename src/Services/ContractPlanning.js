65a953cae8ea36f8d183dc90a6d5dcf82cd3835b
Jakub Horáček <jakub.horacek@elixeum.com>
2023-08-24 18:50:33 +0200

    Add WIP
    ELXP-5795 Contract Planning External JS Library - JS Functionality

--- src/Services/ContractPlanning.js
+++ src/Services/ContractPlanning.js
@@ -4,10 +4,10 @@
  * @param {object} context - Context object for DI
  */
 export function ContractPlanning(context) {
-    this.logger = context.logger;
-    this.httpClient = context.httpClient;
+  this.logger = context.logger;
+  this.httpClient = context.httpClient;
 
-    this.logger.log("ContractPlanning instance created", "debug");
+  this.logger.log("ContractPlanning instance created", "debug");
 }
 
 /**
@@ -15,8 +15,16 @@ export function ContractPlanning(context) {
  * @param {string} name - Name used in the message.
  */
 ContractPlanning.prototype.ContractModel = function () {
-    this.userId = 0;
-    this.id = 0;
-    this.title = "";
-    this.completed = true;
+  this.userId = 0;
+  this.id = 0;
+  this.title = "";
+  this.completed = true;
+};
+
+/**
+ * ContractDraft model
+ * @param {string} contractDraft - Object containing contract draft data from the form.
+ */
+ContractPlanning.prototype.CreateContractDraftRequest = function (contractDraft) {
+  return this.httpClient.post("/api/ContractPlanning/AddAsync", contractDraft);
 };
