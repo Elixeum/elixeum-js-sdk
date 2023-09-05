cf889d4bd1afcf59bbcb4d7f104a2a9447d2ace7
Jakub Horáček <jakub.horacek@elixeum.com>
2023-09-05 14:14:54 +0200

    Remove hardcoded values and add functions to JS prototype
    ELXP-5795 Contract Planning External JS Library - JS Functionality

--- src/Services/ContractPlanning.js
+++ src/Services/ContractPlanning.js
@@ -22,46 +22,33 @@ ContractPlanning.prototype.ContractModel = function () {
 };
 
 /**
- *  GetNewContract function
- * @param {object} contractData - Object containing contract data from the form.
- * @returns {object} - Object with typeNumber and contractTypeId.
+ *  GetNewContract model
+ * @param {object} dataFromForm - Object containing contract data from the form.
+ * @param {object} dataFromPortal - Object containing contract data from the portal. (custom fields, contract type, etc.)
+ * @returns {object} - Object with typeNumber, contractTypeId and contractFields.
  */
-const getNewContract = (contractData) => {
-  if (contractData?.typeNumber == null) {
-    contractData.typeNumber = 414;
-  }
+ContractPlanning.prototype.GetNewContract = function (dataFromForm, dataFromPortal) {
+  dataFromForm.typeNumber = dataFromPortal.contract?.typeNumber;
+  dataFromForm.contractTypeId = dataFromPortal.contract?.contractTypeId;
+  dataFromForm.contractFields = dataFromPortal.customFields;
 
-  if (contractData?.contractTypeId == null) {
-    contractData.contractTypeId = "52b4807d-aaeb-4522-bd49-b8c3e8375a70";
-  }
-
-  contractData.contractFields = [
-    { customFieldId: "6e69b2ce-18ff-475e-95e6-a09a0bd84de6", value: contractData.rooftop_type },
-    { customFieldId: "8cff81e9-160c-4ebc-bb2d-dd3f931314b8", value: contractData.solar_panels },
-  ];
-
-  if (contractData.contractFields?.length > 0) {
+  if (dataFromForm.contractFields?.length > 0) {
     let index = 0;
-    for (const contractField of contractData.contractFields) {
+    for (const contractField of dataFromForm.contractFields) {
       contractField.order = index;
       index++;
     }
   }
 
-  contractData.customs = {
-    "6e69b2ce-18ff-475e-95e6-a09a0bd84de6": contractData.rooftop_type,
-    "8cff81e9-160c-4ebc-bb2d-dd3f931314b8": contractData.solar_panels,
-  };
-
-  return contractData;
+  return dataFromForm;
 };
 
 /**
- * GetNewDocument function
+ * GetNewDocument model
  * @param {object} contractData - Object containing contract data from the form.
  * @returns {object} - Object with date, currencyCode and contact.
  */
-const getNewDocument = (contractData) => {
+ContractPlanning.prototype.GetNewDocument = function (contractData) {
   let contact = contractData?.contact_information ?? {};
 
   let newDocument = {
@@ -78,9 +65,9 @@ const getNewDocument = (contractData) => {
  * @param {object} contractDraft - Object containing contract draft data from the form.
  * @returns {object} - Object containing formatted contract draft data.
  */
-ContractPlanning.prototype.CreateContractDraftRequest = function (contractDraft) {
-  let newContract = getNewContract(contractDraft);
-  let newDocument = getNewDocument(contractDraft);
+ContractPlanning.prototype.CreateContractDraftRequest = function (dataFromForm, dataFromPortal) {
+  let newContract = this.GetNewContract(dataFromForm, dataFromPortal);
+  let newDocument = this.GetNewDocument(dataFromForm);
 
   let toCreateContract = {
     contract: newContract,
