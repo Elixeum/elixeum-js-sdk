b08ee12dead9990637ec24aad37c96af5c5b5a61
Martin Pernica <martinpernica@outlook.com>
2023-11-10 14:30:24 +0100

    Update Logger with level based methods

--- src/Utilities/Logger.js
+++ src/Utilities/Logger.js
@@ -3,7 +3,7 @@
  * @constructor
  */
 export function Logger() {
-    this.prefix = "Elixeum SDK";
+  this.prefix = "Elixeum SDK";
 }
 
 /**
@@ -12,20 +12,52 @@ export function Logger() {
  * @param {string} level - Log level
  */
 Logger.prototype.log = function (message, level = "info") {
-    switch (level) {
-        case "info":
-            console.info(`${this.prefix} [INFO]: ${message}`);
-            break;
-        case "warn":
-            console.warn(`${this.prefix} [WARN]: ${message}`);
-            break;
-        case "error":
-            console.error(`${this.prefix} [ERROR]: ${message}`);
-            break;
-        case "debug":
-            console.debug(`${this.prefix} [DEBUG]: ${message}`);
-            break;
-        default:
-            console.log(`${this.prefix} [${level.toUpperCase()}]: ${message}`);
-    }
+  switch (level) {
+    case "info":
+      console.info(`${this.prefix} [INFO]: ${message}`);
+      break;
+    case "warn":
+      console.warn(`${this.prefix} [WARN]: ${message}`);
+      break;
+    case "error":
+      console.error(`${this.prefix} [ERROR]: ${message}`);
+      break;
+    case "debug":
+      console.debug(`${this.prefix} [DEBUG]: ${message}`);
+      break;
+    default:
+      console.log(`${this.prefix} [${level.toUpperCase()}]: ${message}`);
+  }
+};
+
+/**
+ * Logs message to console with level "info"
+ * @param {string} message - Message to log
+ */
+Logger.prototype.info = function (message) {
+  this.log(message, "info");
+};
+
+/**
+ * Logs message to console with level "warn"
+ * @param {string} message - Message to log
+ */
+Logger.prototype.warn = function (message) {
+  this.log(message, "warn");
+};
+
+/**
+ * Logs message to console with level "error"
+ * @param {string} message - Message to log
+ */
+Logger.prototype.error = function (message) {
+  this.log(message, "error");
+};
+
+/**
+ * Logs message to console with level "debug"
+ * @param {string} message - Message to log
+ */
+Logger.prototype.debug = function (message) {
+  this.log(message, "debug");
 };
